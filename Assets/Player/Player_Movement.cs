6d2d15d5eab1fb918182942c12c32c741db7c8e0
HungCT <hungcaothinh431@gmail.com>
2024-02-20 23:43:39 +0700

    bug in jump, fall
    1st commit

---
+++ Assets/Player/Player_Movement.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [Header("Component")]
+    protected Rigidbody2D rb;
+    [SerializeField] Animator animator;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    [Header("Movement")]
+    [SerializeField] bool canMove;
+    [SerializeField] float speed;
+    [SerializeField] float currentMovement;
+    [SerializeField] bool isFacingRight;
+    [SerializeField] bool canJump;
+    [SerializeField] float jumpPower;
+    [SerializeField] float jumpTime;
+    [SerializeField] float jumpTimeCounter;
+    void Start()
+    {
+        speed = 10f;
+        jumpPower = 10f;
+        jumpTime = 0.3f;
+        canMove = true;
+        isFacingRight = true;
+        canJump = true;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Running();
+        Jump();
+        Flip();
+        animator.SetBool("IsGround", IsGrounded());
+    }
+    protected void Running()
+    {
+        if (canMove)
+        {
+            currentMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            if (currentMovement != 0)
+            {
+                animator.SetBool("IsRunning", true);
+            }
+            else if (currentMovement == 0)
+            {
+                animator.SetBool("IsRunning", false);
+            }
+            transform.Translate(currentMovement, 0, 0);
+        }
+    }
+    protected void Flip()
+    {
+        if (isFacingRight && currentMovement < 0f || !isFacingRight && currentMovement > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    protected bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.3f, groundLayer);
+    }
+    protected void Jump()
+    {
+        if (rb.velocity.y > 0)
+        {
+            animator.SetBool("IsJump", true);
+            animator.SetBool("IsFall", false);
+
+        }
+        else if (rb.velocity.y < 0)
+        {
+            animator.SetBool("IsJump", false);
+            if (IsGrounded() == false)
+            {
+                animator.SetBool("IsFall", true);
+            }
+            else if (IsGrounded() == true)
+            {
+                animator.SetBool("IsFall", false);
+                animator.SetBool("IsJump", false);
+            }
+        }
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && IsGrounded())
+        {
+            canJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jumpTimeCounter = jumpTime;
+        }
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && canJump == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                canJump = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canJump = false;
+        }
+    }
+}
