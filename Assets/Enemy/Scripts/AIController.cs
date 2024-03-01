cc0b44e6f2d32f20928b9a33c2f19618cf68eb06
zhusian <duongdinhdat6@gmail.com>
2024-03-01 13:44:28 +0700

    cm1
    cm1

---
+++ Assets/Enemy/Scripts/AIController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+    
+{
+    public GameObject Player;
+    public bool flip;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 scale = transform.localScale;
+        if(Player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 :1);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime *-1, 0, 0);
+
+        }
+        transform.localScale = scale;
+    }
+}
