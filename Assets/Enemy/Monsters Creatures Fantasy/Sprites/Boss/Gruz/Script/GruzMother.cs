fa6b4940abb0c957431692af6667dab958559be5
zhusian <duongdinhdat6@gmail.com>
2024-03-11 14:56:01 +0700

    boss 3

---
+++ Assets/Enemy/Monsters Creatures Fantasy/Sprites/Boss/Gruz/Script/GruzMother.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class GruzMother : MonoBehaviour
+{
+    [Header("Idel")]
+    [SerializeField] float idelMovementSpeed;
+    [SerializeField] Vector2 idelMovementDirection;
+
+    [Header("AttackUpNDown")]
+    [SerializeField] float attackMovementSpeed;
+    [SerializeField] Vector2 attackMovementDirection;
+
+    [Header("AttackPlayer")]
+    [SerializeField] float attackPlayerSpeed;
+    [SerializeField] Transform player;
+    [SerializeField] int damageAmount ;
+
+
+    [Header("Other")]
+    [SerializeField] Transform goundCheckUp;
+    [SerializeField] Transform goundCheckDown;
+    [SerializeField] Transform goundCheckWall;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
+    private bool isTouchingUp;
+    private bool isTouchingDown;
+    private bool isTouchingWall;
+    private bool hasPlayerPositon;
+
+    private Vector2 playerPosition;
+
+    private bool facingLeft = true;
+    private bool goingUp = true;
+    private Rigidbody2D enemyRB;
+    private Animator enemyAnim;
+
+
+    void Start()
+    {
+        idelMovementDirection.Normalize();
+        attackMovementDirection.Normalize();
+        enemyRB = GetComponent<Rigidbody2D>();
+        enemyAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingUp = Physics2D.OverlapCircle(goundCheckUp.position, groundCheckRadius, groundLayer); 
+        isTouchingDown = Physics2D.OverlapCircle(goundCheckDown.position, groundCheckRadius, groundLayer); 
+        isTouchingWall = Physics2D.OverlapCircle(goundCheckWall.position, groundCheckRadius, groundLayer);
+    }
+
+    void RandomStatePicker()
+    {
+        int randomState = Random.Range(0, 2);
+        if (randomState == 0)
+        {
+            enemyAnim.SetTrigger("AttackUpNDown");
+        }
+        else if (randomState == 1)
+        {
+            enemyAnim.SetTrigger("AttackPlayer");
+        }
+    }
+
+   public void IdelState()
+    {
+        if (isTouchingUp && goingUp)
+        {
+            ChangeDirection();
+        }
+        else if (isTouchingDown && !goingUp)
+        {
+            ChangeDirection();
+        }
+
+        if (isTouchingWall)
+        {
+            if (facingLeft)
+            {
+                Flip();
+            }
+            else if (!facingLeft)
+            {
+                Flip();
+            }
+        }
+        enemyRB.velocity = idelMovementSpeed * idelMovementDirection;
+    } 
+   public void AttackUpNDownState()
+    {
+        if (isTouchingUp && goingUp)
+        {
+            ChangeDirection();
+        }
+        else if (isTouchingDown && !goingUp)
+        {
+            ChangeDirection();
+        }
+
+        if (isTouchingWall)
+        {
+            if (facingLeft)
+            {
+                Flip();
+            }
+            else if (!facingLeft)
+            {
+                Flip();
+            }
+        }
+        enemyRB.velocity = attackMovementSpeed * attackMovementDirection;
+    }
+
+    public void AttackPlayerState()
+    {
+       
+        if (!hasPlayerPositon)
+        {
+            FlipTowardsPlayer();
+             playerPosition = player.position - transform.position;
+            playerPosition.Normalize();
+            hasPlayerPositon = true;
+        }
+        if (hasPlayerPositon)
+        {
+            enemyRB.velocity = attackPlayerSpeed * playerPosition;
+           
+        }
+        
+
+        if (isTouchingWall || isTouchingDown)
+        {
+            //play Slam animation
+            enemyAnim.SetTrigger("Slamed");
+            enemyRB.velocity = Vector2.zero;
+            hasPlayerPositon = false;
+        }
+    }
+
+    void FlipTowardsPlayer()
+    {
+        float playerDirection = player.position.x - transform.position.x;
+
+        if (playerDirection>0 && facingLeft)
+        {
+            Flip();
+        }
+        else if (playerDirection<0 && !facingLeft)
+        {
+            Flip();
+        }
+    }
+
+    void ChangeDirection()
+    {
+        goingUp = !goingUp;
+        idelMovementDirection.y *= -1;
+        attackMovementDirection.y *= -1;
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        idelMovementDirection.x *= -1;
+        attackMovementDirection.x *= -1;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(goundCheckUp.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(goundCheckDown.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(goundCheckWall.position, groundCheckRadius);
+    }
+    
+    private void OnCollisionEnter2(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") )
+        {
+           //
+        }
+    }
+}
